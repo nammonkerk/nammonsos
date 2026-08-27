116b5e6622d8bd3e78fc86f32950c7186349d76c
nammonkerk <6821901012@cdti.ac.th>
2026-08-27 09:55:24 +0700

    update

---
+++ app/admin/blogs/page_x.tsx
@@ -0,0 +1,11 @@
+import BlogsCard from "@/components/BlogCard";
+import BlogForm from "@/components/BlogForm";
+
+export default function BlogPage() {
+    return (
+        <div>
+            <BlogForm />
+            <BlogsCard/>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     
       <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
       <p> โดย ศุภณัฐ เกริกชัยวัน </p>
-      <BlogsCard/>
+    
     </div>
   );
 }
